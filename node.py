c96cc258c704959d59cd2a1140691b8deb382b4c
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2018-03-09 12:23:27 +0330

    add fn to node for A*

--- node.py
+++ node.py
@@ -1,16 +1,19 @@
 class Node:
-    def __init__(self, state = None, parent = None, path_cost = None):
+    def __init__(self, state = None, parent = None, path_cost = None, fn = None):
         self.cell = state
         self.parent = parent
         self.path_cost = path_cost
+        self.fn = fn
 
     def __str__(self):
         val = 'cell = (%d, %d)\n' % self.cell
-        if (self.parent is not None):
+        if self.parent is not None:
             val += 'parent_cell = (%d, %d)\n' % self.parent.cell
         else:
             val += 'No parent\n'
         val += 'path_cost = %d\n' % (self.path_cost)
+        if self.fn is not None:
+            val += 'f(n) = %d\n' %(self.fn)
         return val
 
     def getPath(self):
